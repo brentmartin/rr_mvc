2299b76ece61ef9fc67d498e0380c7b8e1b37aeb
Brent Martin <bmart35@gmail.com>
2016-03-22 17:32:44 -0500

    added rendering for a specific id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
     render Users.new.all_users.to_json
   end
 
-  # def show
-  #   id = params[:id].to_i
-  #   all_users = Users.new.all_users
-  #   if id > all_users.length-1
-  #     render "Error 404 user error!"
-  #   else
-  #     user = all_users[id]
-  #     render user.to_json
-    end
+  def show
+    id = params[:id].to_i
+    all_users = Users.new.all_users
+    # if id > all_users.length-1
+    #   render "Error 404 user error!"
+    # else
+      user = all_users[id]
+      render user.to_json
+    # end
   end
 end
