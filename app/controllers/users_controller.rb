c009a1d247e005ce54afc171c038bdd76744e1c2
Brent Martin <bmart35@gmail.com>
2016-03-23 11:45:24 -0500

    added error throw if id is not in database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
   def show
     id = params[:id].to_i
     all_users = Users.new.all_users
-    # if id > all_users.length-1
-    #   render "Error 404 user error!"
-    # else
+    if id > all_users.length-1
+      render "Error 404 user error!"
+    else
       user = all_users[id]
       render user.to_json
-    # end
+    end
   end
 end
